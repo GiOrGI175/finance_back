87b1c65be6c93f06ba3de38c0e1829e07baaffdd
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-01-16 13:58:29 +0400

    fix/cors

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ const userRouter = require('./routes/users/users.router');
 
 const app = express();
 const cors = require("cors")
+const corsOptions = {
+  origin: 'https://finance-orcin-tau.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 connectToDb();
 
