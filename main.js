20edc06a0b7a9d6ea1588e172e945f935b1f1e6b
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-01-18 21:14:26 +0400

    c

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 const express = require('express');
-// const cors = require('cors');
 const connectToDb = require('./db/connectToMondoDb');
 const usersRouter = require('./routes/users/users.router');
 const potsRouter = require('./routes/pots/pots.router');
 const authRouter = require('./auth/auth.router');
 const userRouter = require('./routes/users/users.router');
-const budgetsRouter = require('./routes/budgets/budgets.router');
+const transactionRouter = require('./routes/transaction/transaction.router');
 
 const app = express();
 const cors = require('cors');
-const corsOptions = {
-  origin: ['https://finance-orcin-tau.vercel.app', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
+const transactionsModel = require('./model/transactions.model');
+const budgetsRouter = require('./routes/budgets/budgets.router');
+
 app.use(express.json());
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use(cors());
 
 connectToDb();
 
@@ -31,6 +27,8 @@ app.use('/budgets', budgetsRouter);
 // });
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
+app.use('/transactions', transactionRouter);
+
 app.listen(3001, () => {
   console.log('running on: http://localhost:3001 ');
 });
