45008f3420808a3444959ca09269172d27b322bf
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-01-18 21:38:51 +0400

    fix/issues

--- main.js
+++ main.js
@@ -1,37 +1,29 @@
 const express = require('express');
 const connectToDb = require('./db/connectToMondoDb');
-const usersRouter = require('./routes/users/users.router');
+// const usersRouter = require('./routes/users/users.router');
 const potsRouter = require('./routes/pots/pots.router');
 const authRouter = require('./auth/auth.router');
 const userRouter = require('./routes/users/users.router');
 const transactionRouter = require('./routes/transaction/transaction.router');
-const express = require('express');
-const connectToDb = require('./db/connectToMondoDb');
-const usersRouter = require('./routes/users/users.router');
-const potsRouter = require('./routes/pots/pots.router');
-const authRouter = require('./auth/auth.router');
-const userRouter = require('./routes/users/users.router');
-const transactionRouter = require('./routes/transaction/transaction.router');
+
 
 const app = express();
 const cors = require('cors');
-const transactionsModel = require('./model/transactions.model');
+// const transactionsModel = require('./model/transactions.model');
 const budgetsRouter = require('./routes/budgets/budgets.router');
 
-const cors = require('cors');
-const transactionsModel = require('./model/transactions.model');
+
 
 app.use(express.json());
 app.use(cors());
-app.use(cors());
+
 
 connectToDb();
 
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 app.use('/pots', potsRouter);
 app.use('/budgets', budgetsRouter);
-app.use('/users', usersRouter);
-app.use('/pots', potsRouter);
+
 
 // app.use('');
 
@@ -42,9 +34,7 @@ app.use('/auth', authRouter);
 app.use('/users', userRouter);
 app.use('/transactions', transactionRouter);
 
-app.use('/auth', authRouter);
-app.use('/users', userRouter);
-app.use('/transactions', transactionRouter);
+
 
 app.listen(3001, () => {
   console.log('running on: http://localhost:3001 ');
