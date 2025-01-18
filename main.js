9c80c30411a51f7712f68e1afb64e35e765a06e2
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-01-18 17:53:00 +0400

    fix/bugs

--- main.js
+++ main.js
@@ -1,43 +1,33 @@
-const express = require('express');
-const connectToDb = require('./db/connectToMondoDb');
-const usersRouter = require('./routes/users/users.router');
-const potsRouter = require('./routes/pots/pots.router');
-const authRouter = require('./auth/auth.router');
-const userRouter = require('./routes/users/users.router');
-const transactionRouter = require("./routes/transaction/transaction.router")
-
+const express = require("express");
+const connectToDb = require("./db/connectToMondoDb");
+const usersRouter = require("./routes/users/users.router");
+const potsRouter = require("./routes/pots/pots.router");
+const authRouter = require("./auth/auth.router");
+const userRouter = require("./routes/users/users.router");
+const transactionRouter = require("./routes/transaction/transaction.router");
 
 const app = express();
 const cors = require("cors");
-const transactionsModel = require('./model/transactions.model');
-const corsOptions = {
-  origin: ['https://finance-orcin-tau.vercel.app' ,'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
+const transactionsModel = require("./model/transactions.model");
+
 app.use(express.json());
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use(cors());
 
 connectToDb();
 
-app.use('/users', usersRouter);
-app.use('/pots', potsRouter);
+app.use("/users", usersRouter);
+app.use("/pots", potsRouter);
 
 // app.use('');
 
 // app.get('/', (req, res) => {
 //   res.send('hi');
 // });
-app.use("/auth",authRouter)
-app.use("/users", userRouter)
-app.use('/transactions', transactionRouter);
-// app.use("/transaction",transactionRouter);
-app.get("/add", async(req,res)=>{
-  await transactionsModel.insertMany()
+app.use("/auth", authRouter);
+app.use("/users", userRouter);
+app.use("/transactions", transactionRouter);
 
-})
 
 app.listen(3001, () => {
-  console.log('running on: http://localhost:3001 ');
+  console.log("running on: http://localhost:3001 ");
 });
