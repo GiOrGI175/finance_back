2e1feed9b963f1f7f485feded10d320531b8095d
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-01-21 16:10:02 +0400

    update

--- db/connectToMondoDb.js
+++ db/connectToMondoDb.js
@@ -1,11 +1,13 @@
 const { default: mongoose } = require('mongoose');
-require('dotenv').config();
+require('dotenv').config(); 
 
 module.exports = async () => {
   try {
-    await mongoose.connect("mongodb+srv://giorgobiani2002:Lashiko20@web10.y4usl.mongodb.net/?retryWrites=true&w=majority&appName=web10");
-    console.log('mongo db connected successfully');
+    
+    
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.log(error);
+    console.log('Error connecting to MongoDB:', error);
   }
 };
